d12a69e5dbf4417066fe0f1e199066b1197cf712
seongssu <loveforggong@gmail.com>
2023-07-20 15:52:20 +0900

    [FEAT] : ⚙️ 이벤트 발생 시 호출될 함수를 구현한 리스너 객체를 생성하여 Button에 지정

--- app/src/androidTest/java/net/flow9/thisiskotlin/calculatorapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/net/flow9/thisiskotlin/calculatorapp/ExampleInstrumentedTest.kt
@@ -13,12 +13,15 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)      //Runner를 명시적으로 지정한다.
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext //를 이용하여 필요한 뷰를 만들어준다.
         assertEquals("net.flow9.thisiskotlin.calculatorapp", appContext.packageName)
+        //assertEquals(A,B) : 객체 A와 B가 같은 값을 가지는지 확인한다.
+        //packageName은 애플리케이션을 구분하는 고유한 값이다. 다른 앱과 구분하는 역할을 하므로 유일무이해야한다.
+        //net.flow9.thisiskotlin.calculatorapp : 현재앱의 이름인가? 이건 모르겠다.
     }
 }
